7d359d50ede05bfb34c744b5f55d4ba4dcb51dbf
Eric Spiegelberg <eric@miletwentyfour.com>
2016-10-26 19:31:31 -0500

    Make use of Cypher parameters, per @ikwattro feedback.
    https://github.com/graphaware/neo4j-uuid/pull/31

--- src/main/java/com/graphaware/module/uuid/generator/SequenceIdGenerator.java
+++ src/main/java/com/graphaware/module/uuid/generator/SequenceIdGenerator.java
@@ -1,5 +1,8 @@
 package com.graphaware.module.uuid.generator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Result;
 
@@ -7,19 +10,23 @@ import com.graphaware.common.uuid.UuidGenerator;
 import com.graphaware.module.uuid.GraphDatabaseServiceAware;
 
 /**
- * A complex implementation of UuidGenerator that makes use of the provided GraphDatabaseService to 
- * generate a numerically increasing sequence through Cypher.
+ * A non-trivial UuidGenerator implementation that makes use of the provided GraphDatabaseService and 
+ * Cypher to generate a numerically increasing sequence.
  */
 public class SequenceIdGenerator implements UuidGenerator, GraphDatabaseServiceAware {
 
-	public static final String cypher = "MERGE (sequenceMetadata:SequenceMetadata) set sequenceMetadata.sequence = coalesce(sequenceMetadata.sequence, 0) + 1 return sequenceMetadata.sequence";
+	public static final String cypher = "MERGE (sequenceMetadata:SequenceMetadata) set sequenceMetadata.sequence = coalesce(sequenceMetadata.sequence, {initialSequennceValue}) + {sequenceIncrementAmount} return sequenceMetadata.sequence";
 	
 	protected GraphDatabaseService database;
 	
 	@Override
 	public String generateUuid() {
 		
-		Result result = database.execute(cypher);
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("initialSequennceValue", 0);
+		parameters.put("sequenceIncrementAmount", 1);
+		
+		Result result = database.execute(cypher, parameters);
 		
 		long sequence = (long) result.next().values().iterator().next();
 		return String.valueOf(sequence);
